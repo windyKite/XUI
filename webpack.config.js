48131b6d5bfd078b477d706cad0221e6c03f644e
xiaoyongsen <603540681@qq.com>
2019-08-13 15:54:01 +0800

    添加 webpack 的 externals 配置,不打包某些外部扩展

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  // mode: 'production',
+  mode: 'production',
   entry: {
     index: './lib/index.tsx'
   },
@@ -27,5 +27,19 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
   }
 }
