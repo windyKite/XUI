57e5060f06c72a5cdd383c5e8bc20e19653625ec
萧泳森 <603540681@qq.com>
2019-08-24 14:58:55 +0800

    jest 测试配置完成

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Button from "./button";
+// import React from "react";
+// import ReactDom from "react-dom";
+// import Button from "./button";
 
-ReactDom.render(<Button />, document.getElementById("root"));
+// ReactDom.render(<Button />, document.getElementById("root"));
+
+import Button from './button';
+
+const a = 1
+console.log('hi')
+
+export {Button, a};
