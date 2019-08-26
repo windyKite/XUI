8481c3b37c960ea5804b7f0d142475f81e022f0f
xiaoyongsen <603540681@qq.com>
2019-08-26 16:53:29 +0800

    动态引入icons目录下的所有svg文件

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import wechat from "./icons/wechat.svg";
+import "./importIcons.js";
 interface IconProps {
   name: string;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
-  return <span>{props.name}</span>;
+  return (
+    <span>
+      <svg>
+        <use xlinkHref={`#${props.name}`} />
+      </svg>
+    </span>
+  );
 };
 
 export default Icon;
