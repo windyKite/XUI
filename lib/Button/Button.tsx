f20ca7ac5445e4816c310bf63cf2c7043fc9af47
萧泳森 <603540681@qq.com>
2019-11-08 17:42:05 +0800

    完成 Button 组件

---
+++ lib/Button/Button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Icon from '../Icon/Icon'
+import classNames from "classnames";
+import "./Button.scss"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: string;
+  appearance?: "default" | "primary" | "link" | "subtle" | "ghost";
+  disabled?: boolean;
+  href?: string;
+  size?: "large" | "small";
+}
+
+const Button: React.FunctionComponent<ButtonProps> = ({
+  className,
+  icon,
+  appearance = "default",
+  color,
+  disabled,
+  href,
+  size,
+  children,
+  ...restProps
+}) => {
+  const classes = classNames("Xui-button", "ripple", className,appearance, color, size, { disabled })
+
+  if(icon){
+    return (
+      <button className={classes} {...restProps}>
+        <Icon name={icon}/>
+        {children}
+      </button>
+    )
+  }else{
+    return (
+      <button className={classes} {...restProps}>
+        {children}
+      </button>
+    )
+  }
+}
+
+export default Button
